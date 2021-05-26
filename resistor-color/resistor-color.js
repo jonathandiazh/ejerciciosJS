a9d7b26b93d2fddd25fadc44dddfd56cde40adda
Jonathan Diaz H <Jonathandiazh25@gmail.com>
2021-05-26 18:28:33 -0500

    primer commit

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,19 @@
+export const colorCode = (color) => {
+  if (color == 'black') return 0;
+  if (color == 'white') return 9;
+  if (color == 'orange') return 3;
+  return -1;
+};
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
